b10134dcd923486596959b5406b7a39408572f50
Chanu <kollichanu@gmail.com>
2026-01-05 11:46:24 -0600

    added service files

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if [ -z "$1" ]; then
   echo 'Input is expected'
   exit 1
 fi
-ansible-playbook -i "${1}"-dev.chanakyakolli.online, main.yaml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e component =$1
+ansible-playbook -i "${1}"-dev.chanakyakolli.online, main.yaml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e component=$1
